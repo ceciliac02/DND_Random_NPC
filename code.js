94bd8920e4e22cd052d3a228b51a8b39d15be987
Cecilia Cruz <cruzscecilia@gmail.com>
2021-03-19 15:12:21 -0700

    Changed code to reflect oop

--- code.js
+++ code.js
@@ -1,25 +1,118 @@
 {
     const api = 'https://www.dnd5eapi.co/api/races';
-    const race = document.getElementById("race");
     const prop = document.querySelectorAll(".properties");
     const lorem = document.getElementById("lorem");
 
-    //The number of the selected race converted to variable for easier use
-    const selectedNumber = randomRace();
+    
 
-    //Gets api data
+    class NPC {
+        constructor(race, height, gender, hair, skinColor, trait1, trait2, trait3, profession) {
+            this.race = race;
+            this.height = height;
+            this.gender = gender;
+            this.hair = hair;
+            this.skinColor = skinColor;
+            this.trait1 = trait1;
+            this.trait2 = trait2;
+            this.trait3 = trait3;
+            this.profession = profession;
+        }
+    }
+
+    fetch (api)
+        .then (response => {
+            return response.json();
+        })
+        .then (data => {
+            const results = data.results;
+            //Grabs a race index from the api through a random number
+            const raceIndex = randomRace(results.length);
+            const race = results[raceIndex].name;
+            //
+            const height = heightModifier(race);
+            //
+            const gender = genderModifier();
+            //Sets the attributes for the new random NPC instance
+            let randomNPC = new NPC(race, height, gender);
+            console.log(randomNPC);
+            console.log(heightModifier("Human"));
+        })
+
+        //Randomizes a number based on the number of races available
+        const randomRace = racesCount => {
+            return Math.floor(Math.random() * (racesCount + 1));
+        }
+
+        function heightModifier (race) {
+            let heightFeet;
+            let heightInches = Math.floor(Math.random() * 10) + 1;
+            let height;
+            if (race === "Dwarf") {
+                heightFeet = Math.floor(Math.random() * 4) + 1;
+                if (heightFeet < 4) {
+                    heightFeet = 4;
+                }
+            }
+            else if (race === "Gnome" || race === "Halfling") {
+                heightFeet = Math.floor(Math.random() * 3) + 1;
+                if (heightFeet < 3) {
+                    heightFeet = 3;
+                }
+            }
+            else if (race === "Elf" || race === "Human" || race === "Half-Elf") {
+                heightFeet = Math.floor(Math.random() * 5) + 1;
+                if (heightFeet < 5) {
+                    heightFeet = 5;
+                }
+            }
+            else if(race === "Half-Orc" || race === "Dragonborn") {
+                heightFeet = Math.floor(Math.random() * 6) + 1;
+                if (heightFeet < 6) {
+                    heightFeet = 6;
+                }
+            }
+            height = heightFeet + "'" + heightInches + "\"";
+            return height;
+        }
+
+        function genderModifier () {
+            let gender;
+            const number = Math.floor(Math.random() * 500) + 1;
+            if (number < 200) {
+                gender = "female";
+            }
+            else if (number > 200 && number < 400) {
+                gender = "male";
+            }
+            else if (number > 400 && number < 450) {
+                gender = "non-binary";
+            }
+            else {
+                gender = "genderfluid";
+            }
+            return gender;
+        }
+
+
+    /*//Gets api data
     fetch (api) 
         .then (response => {
             return response.json();
         })
         .then (data => {
             const results = data.results;
+            //The number of the selected race converted to variable for easier use
+            const selectedNumber = randomRace();
             //The index of the chosen race
             const index = results[selectedNumber].index;
+            const race = results[selectedNumber].name;
+            const height = heightModifier(index);
+            const gender = genderModifier();
+            let randomNPC = new NPC(race, height, gender);
+            console.log(randomNPC);
             displayRace(results[selectedNumber].name);
-            let height = heightModifier(index);
             displayHeight(height); 
-            displayGender(genderModifier());      
+            displayGender(gender);      
         })
 
         //Rolls for a random number for the race index
@@ -85,7 +178,7 @@
        //Modifies the gender of the NPC
         function genderModifier () {
             let gender;
-            const number = Math.floor(Math.random() * 600) + 1;
+            const number = Math.floor(Math.random() * 500) + 1;
             if (number < 200) {
                 gender = "female";
             }
@@ -95,18 +188,21 @@
             else if (number > 400 && number < 450) {
                 gender = "non-binary";
             }
-            else if (number > 450 && number < 500) {
-                gender = "trans-male";
-            }
-            else if (number > 500 && number < 550) {
-                gender = "trans-female";
-            }
             else {
                 gender = "genderfluid";
             }
             return gender;
         }
-        
 
+        function hairModifier (race) {
+            let hairLength;
+            let hairColors = ["auburn", "brown", "black", "red", "white", "gray", "blonde", "teal", "green", "purple", "lilac", "maroon", "silver", "navy", "pink"];
+            
+            length: short, middle, long
+            color: auburn, brown, black, red, white, gray, blonde, teal, 
+            
+        }
+        
+        */
 
 }
